251e18278d6501b9b42529aa469dd2229696f159
admin <kvitka.ok@gmail.com>
2015-07-23 20:45:53 +0300

    first 3 lessons

---
+++ lesson1/src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("QA Factory");
+        int x = 10;
+        int a;
+        a = 15;
+        System.out.println(a);
+        a = 15 + 27;
+        System.out.println(a);
+
+        System.out.println(a);
+
+        a = a + 1;
+        System.out.println(a);
+
+        a = x + a + 15 + x * 2;
+        System.out.println(a);
+
+
+        String s;
+        s = "hello class";
+        System.out.println(s);
+        int g = 288;
+        System.out.println(s + g);
+        double h = 10.5;
+        h = h / 3;
+        System.out.println(10 / 3.0);
+        boolean o;
+        o = true;
+        o = true || false;
+
+        System.out.println(10 > 5);
+        int[] m = {1, 10, 20};
+        System.out.println(m[1]);
+        String[] m1 = {"hello", "class"};
+        System.out.println(m1[0] + " " + m1[1]);
+
+        int b = 10;
+        if (a < 10) {
+            System.out.println(" a < 10");
+        } else {
+            System.out.println(" a> = 10");
+        }
+
+            int a1  = 0, a2 = 1, a3  = 0, a4 = 0;
+            while ( a4<= 20){
+                System.out.println(a1 +  " ");
+                a3 = a1;
+                a1 = a2;
+                a2 = a1 + a3;
+                a4 = a4 + 1;}
+        for ( int a11= 0; a11< 10; a11= a11 + 5){
+            System.out.println (a11);
+            //for  ( String z = "Hello Class" + " " + "Hello Class")
+            }
+        }
+    }
+
+
+
