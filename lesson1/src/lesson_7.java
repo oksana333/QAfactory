2b01fe2c8f6eec8990bcb66698cfe763ed6324c0
admin <kvitka.ok@gmail.com>
2015-08-06 21:14:37 +0300

    first 5 lessons web driver

---
+++ lesson1/src/lesson_7.java
@@ -0,0 +1,24 @@
+/**
+ * Created by admin on 06.08.15.
+ */
+public class lesson_7 {
+    public static void main() {
+        int a = 10;
+        System.out.println(a);
+    }
+
+    public static void func() {
+        System.out.println(a);
+    }
+
+    public static int a = 10;
+
+    public static void func2()
+
+    {
+        int a =30;
+        System.out.println(a);
+
+    }
+}
+
---
+++ lesson1/src/lesson_7_2.java
@@ -0,0 +1,34 @@
+/**
+ * Created by admin on 06.08.15.
+ */
+public class lesson_7_2 {
+    private static void f() {
+        System.out.println(" Private");
+    }
+
+    public static void main3(String[] args) {
+        f();
+    }
+
+
+    public static void main2() {
+        int[] b = {1, 10, -20, 30};
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i]);
+        }
+;
+for (int a : b) {
+        }
+        }
+
+public static void main(String[] args) {
+    int a=10;
+    //a == 10 ? System.out.print("Allright//"): System.out.print("Bad");
+    int b = 2;
+    a = b == 2 ? 7 : 11;
+    System.out.println(a);
+}
+
+}
+
+
